e9b9268bffae3ef211523e5ffba31748602b67a8
Ulku Arikanoglu <arikanogluulku@gmail.com>
2021-11-14 17:36:51 +0300

    react project setup with webpack+babel+eslint

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: path.resolve(__dirname, './src/index.jsx'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(css|s[ac]ss)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'],
+    alias: {
+      Utilities: path.resolve(__dirname, 'src/utils/'),
+    },
+  },
+  devtool: 'eval-cheap-module-source-map',
+  plugins: [
+    new CleanWebpackPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.ProgressPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Contact App',
+      template: path.resolve(__dirname, './public/index.html'),
+    }),
+  ],
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
+  devServer: {
+    compress: true,
+    port: 8080,
+    open: true,
+    historyApiFallback: true,
+  },
+};
