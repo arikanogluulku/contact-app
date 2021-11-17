1400a0f27765a94219d9f3a54537922aa7cd5fa3
Ulku Arikanoglu <arikanogluulku@gmail.com>
2021-11-17 18:42:12 +0300

    contact crud redux,redux-thunk.

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store/store';
 import App from './App';
+import 'alertifyjs/build/css/alertify.css';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
