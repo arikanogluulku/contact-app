353ac8513b9134d92517668dd6cd9c59ba40362f
Ulku Arikanoglu <arikanogluulku@gmail.com>
2021-11-18 21:44:14 +0300

    for all forms used control item

--- src/components/Form/FormControlItem.jsx
+++ src/components/Form/FormControlItem.jsx
@@ -4,7 +4,7 @@ import FloatingLabel from 'react-bootstrap/FloatingLabel';
 
 function FormControlItem({ label, ...props }) {
   return (
-    <FloatingLabel label={label} className="mb-3">
+    <FloatingLabel label={label} className="mb-3 fw-bold text-primary">
       <Form.Control required {...props} />
     </FloatingLabel>
   );
